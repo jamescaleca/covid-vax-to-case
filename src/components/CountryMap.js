68d3e6bcf599612b39e38fc9a3a265e1ac6b4199
James Caleca <jacaleca2@gmail.com>
2022-02-07 22:19:48 -0700

    I think this is the fifth time I've tried a netlify deploy

--- src/components/CountryMap.js
+++ src/components/CountryMap.js
@@ -58,7 +58,7 @@ export default function CountryMap() {
         <div className='map' >
             <GoogleMapReact
                 bootstrapURLKeys={{ 
-                    key: `{process.env.REACT_APP_GOOGLE_API_KEY}`,
+                    key: `'{process.env.REACT_APP_GOOGLE_API_KEY}'`,
                     libraries: ['visualization'] 
                 }}
                 defaultCenter={{
